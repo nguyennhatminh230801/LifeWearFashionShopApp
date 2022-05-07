6980ea38c44cecfa855e51f5365ffbf405c3d4b3
= <=>
2022-05-07 16:07:08 +0700

    add fragment navigation to bottom navigation

--- app/src/main/java/com/nguyennhatminh614/lifewearfashionshopapp/View/HomeActivity.java
+++ app/src/main/java/com/nguyennhatminh614/lifewearfashionshopapp/View/HomeActivity.java
@@ -1,8 +1,12 @@
 package com.nguyennhatminh614.lifewearfashionshopapp.View;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -10,6 +14,11 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.nguyennhatminh614.lifewearfashionshopapp.R;
+import com.nguyennhatminh614.lifewearfashionshopapp.View.home_activity_tab_screen.ContactFragment;
+import com.nguyennhatminh614.lifewearfashionshopapp.View.home_activity_tab_screen.FavoriteFragment;
+import com.nguyennhatminh614.lifewearfashionshopapp.View.home_activity_tab_screen.HomeFragment;
+import com.nguyennhatminh614.lifewearfashionshopapp.View.home_activity_tab_screen.ShoppingFragment;
+import com.nguyennhatminh614.lifewearfashionshopapp.View.home_activity_tab_screen.UserProfileFragment;
 import com.nguyennhatminh614.lifewearfashionshopapp.databinding.ActivityHomeBinding;
 
 public class HomeActivity extends AppCompatActivity {
@@ -22,14 +31,39 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_shopping, R.id.navigation_favorite, R.id.navigation_contact, R.id.navigation_user_profile)
-                .build();
+        loadFragment(new HomeFragment());
 
-        NavController navController = Navigation.findNavController(HomeActivity.this, R.id.nav_host_fragment_activity_home);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+        navView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.navigation_contact:
+                        loadFragment(new ContactFragment());
+                        return true;
+                    case R.id.navigation_home:
+                        loadFragment(new HomeFragment());
+                        return true;
+                    case R.id.navigation_favorite:
+                        loadFragment(new FavoriteFragment());
+                        return true;
+                    case R.id.navigation_shopping:
+                        loadFragment(new ShoppingFragment());
+                        return true;
+                    case R.id.navigation_user_profile:
+                        loadFragment(new UserProfileFragment());
+                        return true;
+                }
+
+                return false;
+            }
+        });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        // load Fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.nav_host_fragment_activity_home, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
